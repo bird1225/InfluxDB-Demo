9a8657134236549fe8b89de4eb29096bec5a65db
Eric <540914848@qq.com>
2021-12-17 11:20:02 +0800

    首次提交

---
+++ src/main/java/com/wanglingfeng/influxdbdemo/InfluxDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wanglingfeng.influxdbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfluxDbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfluxDbDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanglingfeng/influxdbdemo/InfluxDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanglingfeng.influxdbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfluxDbDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
